f9dc221416e6ef196931c62bbbd52dbbf36f5230
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2022-05-10 10:50:39 -0500

    skeleton

---
+++ 03_performance_criteria/03_calc_performance.R
@@ -0,0 +1,16 @@
+library(simhelpers)
+library(tidyverse)
+
+#------------------------------------------------------
+# Calculate performance measures
+# (For some simulations, it may make more sense
+# to do this as part of the simulation driver.)
+#------------------------------------------------------
+
+# want to calcualte rmse, bias, balance...
+# use simhelpers to get mcse
+
+calc_performance <- function(results, model_params) {
+  
+  return(performance_measures)
+}
