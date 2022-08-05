61fb25d4ee00ea46c9b10a7cdc7c619c62cd1889
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2022-08-05 16:30:08 -0500

    test run with 10 iterations

--- 04_run_sim_study/04_run_sim_study.R
+++ 04_run_sim_study/04_run_sim_study.R
@@ -290,7 +290,7 @@ design_factors <- list(
 params <-
   cross_df(design_factors) %>%
   mutate(
-    iterations = 2, # change this to how many ever iterations
+    iterations = 10, # change this to how many ever iterations
     seed = round(runif(1) * 2^30) + 1:n()
   )
 
