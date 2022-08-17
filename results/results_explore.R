60bb2966899c5d6b3c96f1aae15bcc712f976785
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2022-08-17 11:40:49 -0500

    graph prelim

--- results/results_explore.R
+++ results/results_explore.R
@@ -1,15 +1,27 @@
 library(tidyverse)
 
 load("results/simulation_results_mj_1.RData")
+results_mj_1 <- results
 
-results_mj_1 <- results %>%
+load("results/simulation_results_mc1.RData")
+results_mc_1 <- results
+
+results_clean <- bind_rows(results_mj_1, results_mc_1) %>%
+  group_by(method, k, j, icc3, icc2) %>%
+  summarize_at(vars(bias:prop_t_stud_m), mean) %>%
+  ungroup() %>%
+  mutate(K = 400)
+
+
+results_clean <- 
+  results_clean %>%
   mutate(method = as.character(method)) %>%
   mutate(method = factor(method, levels = c("0.1", "0.2", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"))) %>%
   mutate(k_j = paste("k = ", k, ",", "j = ", j),
          icc = paste("icc3 = ", icc3, ",", "icc2 = ", icc2))
 
 
-ggplot(results_mj_1, aes(x = method, y = bias, color = method)) + 
+ggplot(results_clean, aes(x = method, y = bias, color = method)) + 
   geom_point() +
   facet_grid(k_j ~ icc) +
   geom_hline(yintercept = 0, linetype = "dashed") +
