7b331c4ff3ecacbc9362f2fda831fb415c85a539
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2022-08-26 14:08:14 -0500

    graphs

--- results/results_explore.R
+++ results/results_explore.R
@@ -34,8 +34,8 @@ results_clean <-
   mutate(method = as.character(method)) %>%
   filter(!(method %in% c("0.1", "0.2"))) %>%
   mutate(method = factor(method, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"))) %>%
-  mutate(k_j = paste0("k = ", k, ", ", "j = ", j),
-         icc = paste("icc3 = ", icc3, ", ", "icc2 = ", icc2)) %>%
+  mutate(k_j = paste0("# Schools = ", k, ", ", "# Teachers = ", j),
+         icc = paste0("icc3 = ", icc3, ", ", "icc2  = ", icc2)) %>%
   mutate(matching_priority = fct_collapse(method,
                                           "No Distinction" = c("1", "2", "3"),
                                           "Within-site Matching" = c("4", "5", "6"),
@@ -47,34 +47,60 @@ results_clean <-
                                        "Units & Clusters" = c("3", "6", "9", "12"))) 
 
 
+zoom_results <- results_clean %>% 
+  filter(k == 20, icc3 == icc2)
+
+
+# graph function ----------------------------------------------------------
+
+make_plot <- function(dat, 
+                      title,
+                      y_label, 
+                      yint = 0,
+                      type_line = "dashed"){
+  
+  ggplot(dat, aes(x = method, 
+                  y = outcome, 
+                  shape = matching_level, 
+                  color = matching_priority)) + 
+    geom_point() +
+    annotate("rect",
+             xmin = 0, xmax = 3.5,
+             ymin = -Inf, ymax = Inf,
+             fill = "blue", alpha = .2) +
+    annotate("rect",
+             xmin = 6.5, xmax = 9.5,
+             ymin = -Inf, ymax = Inf,
+             fill = "blue", alpha = .2) +
+    facet_grid(icc ~ k_j) +
+    geom_hline(yintercept = yint, linetype = type_line) +
+    scale_color_brewer(palette = "Dark2") +
+    theme_bw() +
+    ggtitle(title) +
+    labs(shape = "", color = "", x = "Method", y = y_label) +
+    theme(legend.position = "bottom")
+  
+}
+
+
+
+
 
 # preliminary graphs ------------------------------------------------------
 
 # bias --------------------------------------------------------------------
 
-# break off into sections
-ggplot(results_clean, aes(x = method, 
-                          y = bias, 
-                          shape = matching_level, 
-                          color = matching_priority)) + 
-  geom_point() +
-  annotate("rect",
-           xmin = 0, xmax = 3.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  annotate("rect",
-           xmin = 6.5, xmax = 9.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  facet_grid(icc ~ k_j) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  ggtitle("Bias") +
-  labs(shape = "", color = "", x = "Method", y = "Bias") +
-  theme(legend.position = "bottom")
+make_plot(dat = results_clean %>% mutate(outcome = bias), 
+          title = "Bias",
+          y_label = "Bias")
 
-ggsave("results/graphs/prelim_graph_bias.png", device = "png", width = 12, height = 8)
+ggsave("results/graphs/full_graph_bias.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome = bias), 
+          title = "Bias",
+          y_label = "Bias")
+
+ggsave("results/graphs/zoom_graph_bias.png", device = "png", width = 12, height = 8)
 
 
 # check_mcse 
@@ -87,29 +113,21 @@ results_clean %>%
 
 # rmse --------------------------------------------------------------------
 
-ggplot(results_clean, aes(x = method, 
-                          y = rmse, 
-                          shape = matching_level, 
-                          color = matching_priority)) + 
-  geom_point() +
-  annotate("rect",
-           xmin = 0, xmax = 3.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  annotate("rect",
-           xmin = 6.5, xmax = 9.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  facet_grid(icc ~ k_j) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  ggtitle("RMSE") +
-  labs(shape = "", color = "", x = "Method", y = "RMSE") +
-  theme(legend.position = "bottom")
 
-ggsave("results/graphs/prelim_graph_rmse.png", device = "png", width = 12, height = 8)
+make_plot(dat = results_clean %>% mutate(outcome = rmse), 
+          title = "RMSE",
+          y_label = "RMSE")
 
+ggsave("results/graphs/full_graph_rmse.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome = rmse), 
+          title = "RMSE",
+          y_label = "RMSE")
+
+ggsave("results/graphs/zoom_graph_rmse.png", device = "png", width = 12, height = 8)
+
+
+     
 # check_mcse 
 # fairly small compared to rmse 
 results_clean %>%
@@ -117,123 +135,132 @@ results_clean %>%
   summarize(rmse_mcse = max(rmse_mcse)) %>%
   arrange(desc(rmse_mcse))
 
-# proportion  ---------------------------------------------------------------
+# proportion teachers  ---------------------------------------------------------------
 
-ggplot(results_clean, aes(x = method, 
-                          y = prop_t_m, 
-                          shape = matching_level, 
-                          color = matching_priority)) + 
-  geom_point() +
-  annotate("rect",
-           xmin = 0, xmax = 3.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  annotate("rect",
-           xmin = 6.5, xmax = 9.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  facet_grid(icc~ k_j) +
-  geom_hline(yintercept = 1, linetype = "dashed") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  ggtitle("Proportion Treatment Teachers Matched") +
-  labs(shape = "", color = "", x = "Method", y = "Proportion of Treated Teachers Matched") +
-  theme(legend.position = "bottom")
+make_plot(dat = results_clean %>% mutate(outcome = prop_t_m), 
+          title = "Proportion of Treated Teachers Matched",
+          y_label = "Proportion of Treated Teachers Matched",
+          yint = 1)
 
+ggsave("results/graphs/full_graph_prop_t_m.png", device = "png", width = 12, height = 8)
 
-ggsave("results/graphs/prelim_graph_prop_t_m.png", device = "png", width = 12, height = 8)
+make_plot(dat = zoom_results %>% mutate(outcome = prop_t_m), 
+          title = "Proportion of Treated Teachers Matched",
+          y_label = "Proportion of Treated Teachers Matched",
+          yint = 1)
+
+ggsave("results/graphs/zoom_graph_prop_t_m.png", device = "png", width = 12, height = 8)
 
 
 
-# proportion  ---------------------------------------------------------------
+# proportion students  ---------------------------------------------------------------
 
-ggplot(results_clean, aes(x = method, 
-                          y = prop_t_stud_m, 
-                          shape = matching_level, 
-                          color = matching_priority)) + 
-  geom_point() +
-  annotate("rect",
-           xmin = 0, xmax = 3.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  annotate("rect",
-           xmin = 6.5, xmax = 9.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  facet_grid(icc ~ k_j) +
-  geom_hline(yintercept = 1, linetype = "dashed") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  ggtitle("Proportion Treatment Teachers Matched - Student Level") +
-  labs(shape = "", color = "", x = "Method", y = "Proportion of Treated Students Matched") +
-  theme(legend.position = "bottom")
+make_plot(dat = results_clean %>% mutate(outcome = prop_t_stud_m), 
+          title = "Proportion of Treated Students Matched",
+          y_label = "Proportion of Treated Students Matched",
+          yint = 1)
 
-ggsave("results/graphs/prelim_graph_prop_t_stud_m.png", device = "png", width = 12, height = 8)
+ggsave("results/graphs/full_graph_prop_t_stud_m.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome = prop_t_stud_m), 
+          title = "Proportion of Treated Students Matched",
+          y_label = "Proportion of Treated Students Matched",
+          yint = 1)
+
+ggsave("results/graphs/zoom_graph_prop_t_stud_m.png", device = "png", width = 12, height = 8)
+
 
 
 # balance -----------------------------------------------------------------
 
-# need to figure out how to graph this better
 
-bal_res <- 
-  results_clean %>%
-  select(method, k_j, icc, U_ijk:Z_k) %>%
-  gather(var, smd, U_ijk:Z_k)
+make_plot(dat = results_clean %>% mutate(outcome = W_jk), 
+          title = "SMD for W_jk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
 
-bal_res %>%
-  ggplot(aes(y = var, x = smd, color = method)) +
-  geom_point() +
-  geom_vline(xintercept = 0) +
-  geom_vline(xintercept = c(-.25, .25), linetype = "dashed") +
-  scale_y_discrete(limits = rev(levels(var))) +
-  facet_grid(icc ~ k_j) +
-  labs(x = "Standardized Mean Differences", y = "") +
-  ggtitle("Covariate Balance") + 
-  theme_bw()
+ggsave("results/graphs/full_graph_smd_W_jk.png", device = "png", width = 12, height = 8)
 
+make_plot(dat = zoom_results %>% mutate(outcome = W_jk), 
+          title = "SMD for W_jk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/zoom_graph_smd_W_jk.png", device = "png", width = 12, height = 8)
 
 
-ggplot(results_clean, aes(x = method, 
-                          y = W_jk, 
-                          shape = matching_level, 
-                          color = matching_priority)) + 
-  geom_point() +
-  annotate("rect",
-           xmin = 0, xmax = 3.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  annotate("rect",
-           xmin = 6.5, xmax = 9.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  facet_grid(icc ~ k_j) +
-  geom_hline(yintercept = 0) +
-  geom_hline(yintercept = .25, linetype = "dashed") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  ggtitle("W_jk") +
-  labs(shape = "", color = "", x = "Method", y = "SMD") +
-  theme(legend.position = "bottom")
 
 
-ggplot(results_clean, aes(x = method, 
-                          y = X_jk, 
-                          shape = matching_level, 
-                          color = matching_priority)) + 
-  geom_point() +
-  annotate("rect",
-           xmin = 0, xmax = 3.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  annotate("rect",
-           xmin = 6.5, xmax = 9.5,
-           ymin = -Inf, ymax = Inf,
-           fill = "blue", alpha = .2) +
-  facet_grid(icc ~ k_j) +
-  geom_hline(yintercept = 0) +
-  geom_hline(yintercept = .25, linetype = "dashed") +
-  scale_color_brewer(palette = "Dark2") +
-  theme_bw() +
-  ggtitle("X_jk") +
-  labs(shape = "", color = "", x = "Method", y = "SMD") +
-  theme(legend.position = "bottom")
+make_plot(dat = results_clean %>% mutate(outcome = X_jk), 
+          title = "SMD for X_jk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/full_graph_smd_X_jk.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome = X_jk), 
+          title = "SMD for X_jk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/zoom_graph_smd_X_jk.png", device = "png", width = 12, height = 8)
+
+
+
+make_plot(dat = results_clean %>% mutate(outcome = X_ijk), 
+          title = "SMD for X_ijk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/full_graph_smd_X_ijk.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome = X_ijk), 
+          title = "SMD for X_ijk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/zoom_graph_smd_X_ijk.png", device = "png", width = 12, height = 8)
+
+
+make_plot(dat = results_clean %>% mutate(outcome = Z_k), 
+          title = "SMD for Z_k",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/full_graph_smd_Z_k.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome = Z_k), 
+          title = "SMD for Z_k",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/zoom_graph_smd_Z_k.png", device = "png", width = 12, height = 8)
+
+
+
+make_plot(dat = results_clean %>% mutate(outcome = U_ijk), 
+          title = "SMD for U_ijk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/full_graph_smd_U_ijk.png", device = "png", width = 12, height = 8)
+
+make_plot(dat = zoom_results %>% mutate(outcome =  U_ijk), 
+          title = "SMD for  U_ijk",
+          y_label = "Standardized Mean Difference",
+          type_line = "solid") +
+  geom_hline(yintercept = .25, linetype = "dashed")
+
+ggsave("results/graphs/zoom_graph_smd_U_ijk.png", device = "png", width = 12, height = 8)
+
+
+
