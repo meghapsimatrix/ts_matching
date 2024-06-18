273f49fba22b283b2eebbce7b3cb0b438cf7cf1d
Megha Joshi <33493716+meghapsimatrix@users.noreply.github.com>
2024-06-18 09:08:32 -0500

    results

--- results/create_tables.R
+++ results/create_tables.R
@@ -1,10 +1,12 @@
+library(tidyverse)
+
 load("results/results/results_clean.RData")
 
 to_table <- results_clean %>%
   filter(icc2 == .1, icc3 == .1) %>%
   select(1:13) %>%
   select(-contains("icc"), -contains("mcse"), -width) %>%
-  mutate_at(vars(bias, rmse, coverage), round, 3)
+  mutate_at(vars(bias, rmse, coverage), round, 2)
 
 bias_res <- to_table %>%
   mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
@@ -17,3 +19,138 @@ bias_res <- to_table %>%
 
 
 write_csv(bias_res, "results/bias_res.csv")
+
+to_table <- results_clean %>%
+  filter(icc2 == .1, icc3 == .2) %>%
+  select(1:13) %>%
+  select(-contains("icc"), -contains("mcse"), -width) %>%
+  mutate_at(vars(bias, rmse, coverage), round, 2)
+
+bias_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:coverage) %>%
+  filter(criteria == "bias") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(bias_res, "results/bias_res_1_2.csv")
+
+
+to_table <- results_clean %>%
+  filter(icc2 == .2, icc3 == .1) %>%
+  select(1:13) %>%
+  select(-contains("icc"), -contains("mcse"), -width) %>%
+  mutate_at(vars(bias, rmse, coverage), round, 2)
+
+bias_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:coverage) %>%
+  filter(criteria == "bias") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(bias_res, "results/bias_res_2_1.csv")
+
+to_table <- results_clean %>%
+  filter(icc2 == .2, icc3 == .2) %>%
+  select(1:13) %>%
+  select(-contains("icc"), -contains("mcse"), -width) %>%
+  mutate_at(vars(bias, rmse, coverage), round, 2)
+
+bias_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:coverage) %>%
+  filter(criteria == "bias") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(bias_res, "results/bias_res.csv")
+
+
+
+# for the x  --------------------------------------------------------------
+
+
+to_table <- results_clean %>%
+  filter(icc2 == .1, icc3 == .1) %>%
+  select(1:5, bias, X_ijk) %>%
+  select(-contains("icc")) %>%
+  mutate_at(vars(bias, X_ijk), round, 2)
+
+smd_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:X_ijk) %>%
+  filter(criteria == "X_ijk") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(smd_res, "results/smd_res_1_1.csv")
+
+to_table <- results_clean %>%
+  filter(icc2 == .1, icc3 == .2) %>%
+  select(1:5, bias, X_ijk) %>%
+  select(-contains("icc")) %>%
+  mutate_at(vars(bias, X_ijk), round, 2)
+
+smd_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:X_ijk) %>%
+  filter(criteria == "X_ijk") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(smd_res, "results/smd_res_1_2.csv")
+
+to_table <- results_clean %>%
+  filter(icc2 == .2, icc3 == .1) %>%
+  select(1:5, bias, X_ijk) %>%
+  select(-contains("icc")) %>%
+  mutate_at(vars(bias, X_ijk), round, 2)
+
+smd_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:X_ijk) %>%
+  filter(criteria == "X_ijk") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(smd_res, "results/smd_res_2_1.csv")
+
+
+
+to_table <- results_clean %>%
+  filter(icc2 == .2, icc3 == .2) %>%
+  select(1:5, bias, X_ijk) %>%
+  select(-contains("icc")) %>%
+  mutate_at(vars(bias, X_ijk), round, 2)
+
+smd_res <- to_table %>%
+  mutate(condition = paste0("k = ", k, ", ", "j = ", j)) %>%
+  select(-k, -j) %>%
+  gather(criteria, val, bias:X_ijk) %>%
+  filter(criteria == "X_ijk") %>%
+  mutate(criteria = paste(condition, criteria)) %>%
+  select(- condition) %>%
+  spread(criteria, val)
+
+
+write_csv(smd_res, "results/smd_res_2_2.csv")
+
